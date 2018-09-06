6e08eede07211b93494d0417fcff9978e8901f0b
NAKAO Kazuyoshi <nakao4532@gmail.com>
2018-09-06 13:33:33 +0900

    Add request Google OpenID

--- openId/client/js/login.js
+++ openId/client/js/login.js
@@ -1,5 +1,19 @@
-$(document).ready(function () {
-    $("#login").click(function () {
-        window.location.href = '../pages/index.html';
+function googleOpenIdRequest () {
+    const openIdUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+    const queryObj = {
+        client_id : '',
+        response_type : 'code',
+        scope : 'openid email',
+        redirect_uri : 'http://localhost:3000/pages/index.html',
+    }
+    const queryStr = JSON.stringify(queryObj)
+    $.get(openIdUrl, encodeURI(queryStr), function (data, status, jqXHR) {
+        console.log("data =" + data);
+        console.log("status = " + status);
+        console.log("jqXHR = " + jqXHR);
     });
+}
+
+$(document).ready(function () {
+    $("#login").click(googleOpenIdRequest);
 });
